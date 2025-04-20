8c4d2d3d170175b4f8aafdb571a0f989a699777b
Hemant kumar shakyawal <hemantkumarshakyawal@gmail.com>
2025-04-20 19:22:44 +0530

    create controller,endpont

---
+++ Employee-Management-Api/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using Employee_Management_Api.Models;
+using Employee_Management_Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
+
+namespace Employee_Management_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController:ControllerBase
+    {
+
+        private readonly IEmpolyeeRepository _employeeRepository;
+
+        public EmployeeController(IEmpolyeeRepository empolyeeRepository)
+        {
+            _employeeRepository = empolyeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployeeasync()
+        {
+            var allEmployee=await _employeeRepository.GetAllAsync();
+            return Ok(allEmployee);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>>GetEmployeeById(int id)
+        {
+
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null) {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new
+            {
+                id = employee.Id
+            }, employee);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployeeById(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
+    }
+}
--- Employee-Management-Api/Program.cs
+++ Employee-Management-Api/Program.cs
@@ -1,4 +1,5 @@
 using Employee_Management_Api.Data;
+using Employee_Management_Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory;
 
@@ -11,25 +12,39 @@ namespace Employee_Management_Api
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<AppDbContext>(
-
-                options => options.UseInMemoryDatabase("EmployeeDb")
-                );
-
+                 options => options.UseInMemoryDatabase("EmployeeDb")
+             );
             builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
 
-           {
-               options.AddPolicy("MyCors", builder =>
-               builder.WithOrigins("http://localhost:4200")
-               .AllowAnyMethod()
-               .AllowAnyHeader()
-               );
-           }
-            );
+            // add the employee repository to DI(dependancy injection)
+            builder.Services.AddScoped<IEmpolyeeRepository,EmpolyeeRepository>();
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
 
             app.UseCors("MyCors");
-            app.MapGet("/", () => "Hello World!");
+          
+            app.MapControllers();
 
             app.Run();
         }
